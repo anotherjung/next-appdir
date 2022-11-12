cee9244afd7f4ab5306d70e92a163eec015198a5
Jung No <jung@jungno.com>
2022-11-11 19:05:40 -0600

    4

--- utils/generate-nonce.ts
+++ utils/generate-nonce.ts
@@ -1,8 +1,9 @@
 // utils/generate-nonce.ts
-import crypto from 'crypto';
+//import crypto from 'crypto';
 
 const generateNonce = (): string => {
   return self.crypto.randomUUID();
+  //return self.crypto.randomBytes(16).toString('base64');
 };
 
 export default generateNonce;
